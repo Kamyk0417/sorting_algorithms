7e2f2345715afd7ca35bd2ce67241ce216720bbd
Kamyk0417 <104413024+Kamyk0417@users.noreply.github.com>
2024-03-28 11:17:49 +0100

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -94,7 +94,7 @@ def bucket_sort(arr):
     return sorted
 
 
-def heapify(arr,n,i):
+def one_heap_sort(arr,n,i):
     largest = i
     left = 2*i+1
     right = 2*i+2
@@ -104,14 +104,14 @@ def heapify(arr,n,i):
         largest = right
     if largest != i:
         arr[i], arr[largest] = arr[largest], arr[i]
-        heapify(arr, n, largest)
+        one_heap_sort(arr, n, largest)
 
 def heap_sort(arr):
     n = len(arr)
     for i in range(n//2-1,-1,-1):
-        heapify(arr,n,i)
+        one_heap_sort(arr,n,i)
     for i in range(n-1,0,-1):
         arr[i], arr[0] = arr[0], arr[i]
-        heapify(arr,i,0)
+        one_heap_sort(arr,i,0)
     
 
